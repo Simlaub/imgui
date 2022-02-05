8d4eda1a70253af7c173f45a880684fa41d55f07
Simon Laubersheimer <s.laubersheimer@t-online.de>
2022-02-05 14:38:50 +0100

    changed premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,8 @@
 project "ImGui"
 	kind "StaticLib"
 	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -21,9 +23,12 @@ project "ImGui"
 	}
 
 	filter "system:windows"
-	systemversion "latest"
-	cppdialect "C++17"
-	staticruntime "On"
+	    systemversion "latest"
 
-	filter { "system:windows", "configurations:Release" }
-		buildoptions "/MT"
+    filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
